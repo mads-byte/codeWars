7e871d422fed16dd21d02831c43492438a3ef1f6
mads-byte <mbutl224@icloud.com>
2025-08-17 16:09:04 -0400

    final solution for challenge 8

---
+++ challenge8.js
@@ -0,0 +1,38 @@
+function switchItUp(number) {
+    let result
+    switch (number) {
+        case 1:
+            result = "One"
+            break;
+        case 2:
+            result = "Two"
+            break;
+        case 3:
+            result = "Three"
+            break;
+        case 4:
+            result = "Four"
+            break;
+        case 5:
+            result = "Five"
+            break;
+        case 6:
+            result = "Six"
+            break;
+        case 7:
+            result = "Seven"
+            break;
+        case 8:
+            result = "Eight"
+            break;
+        case 9:
+            result = "Nine"
+            break;
+        case 0:
+            result = "Zero"
+            break;
+
+    }
+    return result;
+
+};
